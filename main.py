aa6a3fb17899577f33fc2665c1def23b59538e00
323damilola <damilolaogunbayo323@gmail.com>
2024-01-11 13:16:48 +0100

    Updated

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import sys
+
+from flask_cors import CORS
+
+from hairArtProject import create_app
+
+print(sys.path)
+
+app = create_app()
+CORS(app)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
